fb3221cc579dce978102c4ba1590bc47cfc391a1
Saadallah-design <saadalla.publishing@gmail.com>
2025-09-14 05:09:29 +0700

    initial commit for future age calculator

---
+++ python_introduction/future_age_calculator.py
@@ -0,0 +1,8 @@
+# Objective: Practice receiving user input in Python and perform a simple arithmetic operation 
+# to calculate the user’s age in a future year.
+
+
+user_age = int(input("How old are you? "))
+
+user_age_future = user_age + 27
+print(f"In 2050, you will be {user_age_future} years old.")
