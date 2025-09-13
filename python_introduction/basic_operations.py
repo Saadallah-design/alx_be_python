ef187c06c8e35f93233bff003e20d6fb765d9921
Saadallah-design <saadalla.publishing@gmail.com>
2025-09-14 04:50:36 +0700

    initial commit for the basic operations using python.

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,12 @@
+# Objective: To practice basic arithmetic operations in Python by performing predefined calculations.
+
+number1 = 10
+number2 = 5
+result_add = number1 + number2
+print(f"Addition of {number1} and {number2} is {result_add} ")
+
+result_substr = number1 - number2
+print(f"Substraction of {number1} and {number2} is {result_substr} ")
+
+result_times = number1 * number2
+print(f"Substraction of {number1} and {number2} is {result_times} ")
