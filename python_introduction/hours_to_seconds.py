5cc3adb03d03395e8fd8d610676d26afbcb57263
Saadallah-design <saadalla.publishing@gmail.com>
2025-09-14 05:02:55 +0700

    using only seconds var to calculate seconds of hours; the hours_to_seconds file.

--- python_introduction/hours_to_seconds.py
+++ python_introduction/hours_to_seconds.py
@@ -1,8 +1,7 @@
 # Objective: Demonstrate understanding of variable assignments and arithmetic operations 
 # by converting a given number of hours into seconds.
 
-hour_seconds = 3600
 hours = 2
-seconds = hour_seconds * hours
+seconds = hours * 3600
 
 print(f"{hours} hour(s) is {seconds} seconds.")
