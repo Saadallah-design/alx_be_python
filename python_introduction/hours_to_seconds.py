5f37d7cce8b3bf7fd45e080278e37fbc35ad29cb
Saadallah-design <saadalla.publishing@gmail.com>
2025-09-14 05:00:54 +0700

    inital commit of the hours_to_seconds file.

---
+++ python_introduction/hours_to_seconds.py
@@ -0,0 +1,8 @@
+# Objective: Demonstrate understanding of variable assignments and arithmetic operations 
+# by converting a given number of hours into seconds.
+
+hour_seconds = 3600
+hours = 2
+seconds = hour_seconds * hours
+
+print(f"{hours} hour(s) is {seconds} seconds.")
