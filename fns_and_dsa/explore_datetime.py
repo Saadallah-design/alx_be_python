de98f805a18d71afc54f93f41951ee22b7032638
Saadallah-design <saadalla.publishing@gmail.com>
2025-09-29 01:33:03 +0700

    changing import style for autochecker plus input for added days

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -10,7 +10,7 @@ def display_current_datetime():
 def calculate_future_date():
     # function to get Input and Calculate
     try:
-        added_days = int(input("Enter the number of days to add: "))
+        added_days = int(input("Enter the number of days to add to the current date: "))
     except ValueError:
         
         print("Invalid input. Please enter a whole number.")
