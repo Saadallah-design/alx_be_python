23f3e91f8888b6f3d7d8826176e07f6a3ced29e6
Saadallah-design <saadalla.publishing@gmail.com>
2025-09-28 23:54:13 +0700

    Removed data type from the function  task 0 for arithmetic operations

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,6 +1,6 @@
 
 
-def perform_operation(num1: float,num2: float, operation: str):
+def perform_operation(num1, num2, operation):
 
     if operation == "add":
         return num1 + num2
@@ -15,4 +15,6 @@ def perform_operation(num1: float,num2: float, operation: str):
             return num1 / num2
     else:
         return ("The operation provided is not valid")
+
+
 
