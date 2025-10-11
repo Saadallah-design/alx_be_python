a66654d47c5d294e8dd506cd98ac7e378e10440d
Saadallah-design <saadalla.publishing@gmail.com>
2025-10-11 23:06:39 +0700

    modifying the test self.calc text

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -11,28 +11,28 @@ class testSimpleCalculator(unittest.TestCase):
     def setUp(self):
         # This method is called before each test
         # It is used to set up any state that is shared across tests
-        self.calculator = SimpleCalculator()
+        self.calc = SimpleCalculator()
 
-    def test_add(self):
+    def test_addition(self):
         # Test the add method
-        self.assertEqual(self.calculator.add(2, 3), 5)
-        self.assertEqual(self.calculator.add(-1, 1), 0)
-        self.assertEqual(self.calculator.add(-1, -1), -2)
+        self.assertEqual(self.calc.add(2, 3), 5)
+        self.assertEqual(self.calc.add(-1, 1), 0)
+        self.assertEqual(self.calc.add(-1, -1), -2)
 
     def test_subtract(self):
         # Test the subtract method
-        self.assertEqual(self.calculator.subtract(5, 3), 2)
-        self.assertEqual(self.calculator.subtract(0, 0), 0)
-        self.assertEqual(self.calculator.subtract(-1, -1), 0)
+        self.assertEqual(self.calc.subtract(5, 3), 2)
+        self.assertEqual(self.calc.subtract(0, 0), 0)
+        self.assertEqual(self.calc.subtract(-1, -1), 0)
 
     def test_multiply(self):
         # Test the multiply method
-        self.assertEqual(self.calculator.multiply(2, 3), 6)
-        self.assertEqual(self.calculator.multiply(-1, 1), -1)
-        self.assertEqual(self.calculator.multiply(-1, -1), 1)
+        self.assertEqual(self.calc.multiply(2, 3), 6)
+        self.assertEqual(self.calc.multiply(-1, 1), -1)
+        self.assertEqual(self.calc.multiply(-1, -1), 1)
 
     def test_divide(self):
         # Test the divide method
-        self.assertEqual(self.calculator.divide(6, 3), 2)
-        self.assertEqual(self.calculator.divide(-6, 3), -2)
-        self.assertIsNone(self.calculator.divide(5, 0))  # Division by zero should return None
+        self.assertEqual(self.calc.divide(6, 3), 2)
+        self.assertEqual(self.calc.divide(-6, 3), -2)
+        self.assertIsNone(self.calc.divide(5, 0))  # Division by zero should return None
