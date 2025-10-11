1db9c5e78777292d5bfa7e875a2fa0fa3910e00b
Saadallah-design <saadalla.publishing@gmail.com>
2025-10-11 19:28:18 +0700

    adding two decimals to display balance

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -43,4 +43,4 @@ class BankAccount:
     
     # Method to display the current account balance
     def display_balance(self):
-        print(f"Current Balance: ${self.account_balance}")
+        print(f"Current Balance: ${self.account_balance:.2f}")
