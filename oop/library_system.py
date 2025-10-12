912518c1929d656627b6431197f2b6b45fb2e07f
Saadallah-design <saadalla.publishing@gmail.com>
2025-10-13 02:05:33 +0700

    adding the library files editing the output

--- oop/library_system.py
+++ oop/library_system.py
@@ -5,7 +5,7 @@ class Book:
         self.author = author
 
     def __str__(self):
-        return f"{self.title} by {self.author}"
+        return f"Book: {self.title} by {self.author}"
 
     def __repr__(self):
         return f"Book: {self.title} by '{self.author}')"
