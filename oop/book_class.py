963c1b64ef3584336a92deeb3ab3bfcfef00e1d6
Saadallah-design <saadalla.publishing@gmail.com>
2025-10-13 01:45:02 +0700

    initial commit for the book class task

---
+++ oop/book_class.py
@@ -0,0 +1,18 @@
+# objective: Create a Python script named book_class.py. 
+# In this script, define a Book class that uses specific magic methods to enhance its functionality. 
+# This class will model a book with attributes for the title, author, and publication year.
+
+class Book:
+    def __init__(self, title, author, year):
+        self.title = title
+        self.author = author
+        self.year = year
+
+    def __str__(self):
+        return f"{self.title} by {self.author}, published in {self.year}"
+
+    def __repr__(self):
+        return f"Book('{self.title}', '{self.author}', {self.year})"
+
+    def __del__(self):
+        print(f"Deleting'{self.title}'")
