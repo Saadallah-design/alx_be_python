c486e21a90f9392f2a58caa59664173480cff5b7
Saadallah-design <saadalla.publishing@gmail.com>
2025-10-13 02:19:41 +0700

    adding the polymorphism file and its demo

---
+++ oop/polymorphism_demo.py
@@ -0,0 +1,26 @@
+# Objective: Enhance your understanding of polymorphism in Python 
+# by creating a set of classes that 
+# demonstrate method overriding and polymorphic behavior.
+
+
+
+class Shape:
+    def area(self):
+        raise NotImplementedError("Subclasses must implement this method")
+
+
+class Rectangle(Shape):
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def area(self):
+        return self.width * self.height
+    
+class Circle(Shape):
+    def __init__(self, radius):
+        self.radius = radius
+
+    def area(self):
+        import math
+        return math.pi * (self.radius ** 2)
